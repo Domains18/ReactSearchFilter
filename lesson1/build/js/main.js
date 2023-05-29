61e236165db73c4666cd355fd5d6d9a1df9161ec
Domains18 <gibsonsgibson88@gmail.com>
2023-05-29 22:05:43 +0300

    basic typescript
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- lesson1/build/js/main.js
+++ lesson1/build/js/main.js
@@ -1,3 +1,7 @@
 "use strict";
 const naming = "hello";
 console.log(naming);
+let a = 12;
+let b = '6';
+let c = 8;
+console.log(c / a);
--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -3,3 +3,9 @@
 const naming: String = "hello"
 
 console.log(naming)
+
+let a = 12;
+let b = '6';
+let c: number = 8
+
+console.log(c/a)
