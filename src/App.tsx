6fe7417676ba9726bebfbfc7698c514008a4aae9
Domains18 <gibsonsgibson88@gmail.com>
2024-01-18 20:11:50 +0300

    electronjs

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { app, Menu } from "electron";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcomescreen from "./screens/Welcomescreen.tsx";
+import Homepage from "./screens/Home/Homepage.tsx";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcomescreen />} />
+        <Route path="/home" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/screens/Home/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-gray-100">
+        Home Page
+      </div>
+    </>
+  )
+}
